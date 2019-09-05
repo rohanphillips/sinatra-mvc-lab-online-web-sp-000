65b7c3a83d990703176f100ce727c2aa0815b995
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-05 21:37:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   post '/result' do
-    myClass = PigLatinizer(params[:text])
+    myClass = PigLatinizer.new(params[:text])
     mySentence = myClass.result
     erb :pig_latin_result
   end
