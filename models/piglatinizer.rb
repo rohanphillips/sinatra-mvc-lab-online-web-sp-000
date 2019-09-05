cc9e946a014c2ab92c6f4829493e920f735fbd93
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-05 21:18:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,18 @@ require_relative 'config/environment'
 
 class PigLatinizer
 
-  def initialize(string)
-    @words = string.split(" ")
+  def initialize(string = "")
+    @sentence = string
+  end
+
+  def self.result
+    result = @sentence.split(" ").map{|word|
+      latinize(word)
+    }
+    result.join(" ")
+  end
+
+  def latinize(word)
+
   end
 end
