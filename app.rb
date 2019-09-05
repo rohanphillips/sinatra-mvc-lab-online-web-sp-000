e0e61ec01187b6af89c4ce77fcd1dbc7dd0da65a
Rohan Phillips <rohan@solutions-insurance.com>
2019-09-05 20:54:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,5 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb : user_input.erb
-  end
 
-  post '/result' do
-    erb : pig_latin_result.erb
-  end
 end
